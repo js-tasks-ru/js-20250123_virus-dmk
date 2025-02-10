c91ec973c40442793345e93c44e4c1dedc944826
DZ <DZ@email>
2025-02-10 10:21:50 -0800

    add NotificationMessage realization

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,63 @@
 export default class NotificationMessage {
+  element;
+
+  constructor(
+    message = "", 
+    { 
+      duration = 3, //default duration, if got nothing
+      type = "" 
+    } = {}
+    ) { 
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.element = this.createElement(this.createTemplate());
+  }
+
+  createElement(template) {
+
+    const elem = document.createElement('div');
+    elem.innerHTML = template;
+    elem.firstElementChild.outerHTML = template;
+    return elem
+  }
+
+createTemplate() {
+  return `
+    <div class="${this.createTypeMessageTemplate()}" style="--value:${this.duration}">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">success</div>
+      <div class="notification-body">
+        ${this.message}
+      </div>
+    </div>
+  </div>
+  `
+}
+
+createTypeMessageTemplate() {
+  if (this.type === 'success' ) return 'notification success' ;
+  if (this.type === 'error' ) return 'notification error' ;
+  return 'notification '
+  
+}
+
+show() {
+  document.body.append(this.element);
+  setTimeout(this.remove,this.duration);
+}
+
+remove() {
+
+  this.element.remove()
+
+}
+
+destroy() {
+
+  this.remove()
+
+}
 
 }

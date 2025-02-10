60b74d8ad3598e54cccdf330d64ad427be269bf6
DZ <DZ@email>
2025-02-10 10:21:50 -0800

    add NotificationMessage realization

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,71 @@
 export default class NotificationMessage {
+  static lastShownMessage = null;
+  element;
+
+  constructor(
+    message = "", 
+    { 
+      duration = 3, //default duration, if got nothing
+      type = "" 
+    } = {}
+    ) { 
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.timerID = undefined;
+    this.element = this.createElement(this.createTemplate());
+  }
+
+  createElement(template) {
+
+    const elem = document.createElement('div');
+    elem.innerHTML = template;
+    return elem.firstElementChild;
+  }
+
+createTemplate() {
+  return `
+    <div class="${this.createTypeMessageTemplate()}" style="--value:${this.duration}">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">success</div>
+      <div class="notification-body">
+        ${this.message}
+      </div>
+    </div>
+  </div>
+  `
+}
+
+createTypeMessageTemplate() {
+  debugger;
+  if (this.type === 'success' ) return 'notification success' ;
+  if (this.type === 'error' ) return 'notification error' ;
+  return 'notification '
+  
+}
+
+show(target = document.body) {
+  if (NotificationMessage.lastShownMessage) this.remove();
+
+  NotificationMessage.lastShownMessage = this;
+  
+  target.append(this.element);
+  this.timerID = setTimeout(() => this.remove(), this.duration);
+}
+
+remove() {
+
+  this.element.remove();
+  NotificationMessage.lastShownMessage = null;
+
+}
+
+destroy() {
+
+  clearTimeout(this.timerID);
+  this.remove();
+
+}
 
 }

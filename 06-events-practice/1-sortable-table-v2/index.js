770d0c16b055bb28c383d3d502705fad73f79479
Dzmitry B <virus.dmk@gmail.com>
2025-02-21 00:05:23 -0800

    add SortableTable V2 realization, task 6.1

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,62 @@
-export default class SortableTable {
-  constructor(headersConfig, {
-    data = [],
-    sorted = {}
-  } = {}) {
+import SortableTableV1 from '../../05-dom-document-loading/2-sortable-table-v1/index.js';
 
+export default class SortableTableV2 extends SortableTableV1 {
+
+  constructor(headersConfig = {
+    id: '',
+    title: '',
+    sortable: false,
+    template: (data = []) => {}}, 
+    {
+      data = [{
+        id: "",
+        title: "",
+        price: 0,
+        sales: 0,
+      }],
+      sorted = {
+              id: headerConfig.find(item => item.sortable).id,
+              order: 'asc'
+            }
+    } = {}, 
+    isSortLocally = true) {
+    
+    super(headersConfig, data);
+    this.isSortLocally = isSortLocally;
+    // this.element = super.element;
+    this.createListeners();
+  }
+
+  sort () {
+    if (this.isSortLocally) {
+      this.sortOnClient();
+    } else {
+      this.sortOnServer();
+    }
+  }
+
+  sortOnClient() {
+    //@todo
+  }
+
+  sortOnServer() {
+    //@TODO
+  }
+
+  createListeners() {
+
+  }
+
+  removeListeners() {
+
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    this.removeListeners();
   }
 }

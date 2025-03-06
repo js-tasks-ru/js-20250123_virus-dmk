826d6f2b43d103332ee6650a3ee5af0290d45849
DZ <DZ@email>
2025-03-05 20:06:12 -0800

    add tooltip realization: events

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -7,10 +7,12 @@ class Tooltip {
     if(Tooltip.inst) return Tooltip.inst;
   
     Tooltip.inst = this;
-    this.createEventListeners();  
+      
   }
   
     initialize () {
+
+      this.createEventListeners();
   
       const el = document.createElement('div');
       el.innerHTML = this.createTooltipTemplate();
@@ -33,38 +35,43 @@ class Tooltip {
 
     createEventListeners() {
 
-      document.body.addEventListener('pointerover', handleDocPointerover);
-      document.body.addEventListener('pointerout', handleDocPointerout);
-      document.body.addEventListener('pointermove', handleDocPointermove);
+      document.body.addEventListener('pointerover', this.handleDocPointerover);
+      document.body.addEventListener('pointerout', this.handleDocPointerout);
+      document.body.addEventListener('pointermove', this.handleDocPointermove);
 
     }
 
     removeEventListeners() {
 
-      document.body.removeEventListener('pointerover', handleDocPointerover);
-      document.body.removeEventListener('pointerout', handleDocPointerout);
-      document.body.removeEventListener('pointermove', handleDocPointermove);
+      document.body.removeEventListener('pointerover', this.handleDocPointerover);
+      document.body.removeEventListener('pointerout', this.handleDocPointerout);
+      document.body.removeEventListener('pointermove', this.handleDocPointermove);
 
     }
 
     handleDocPointerover = (e) => {
+      if (!e.target.closest('[data-tooltip]')) return;
 
+      this.render(e.target.closest('[data-tooltip]').dataset.tooltip);
 
-
+      this.element.style.left = `${e.pageX + 10}px`;
+      this.element.style.top = `${e.pageY + 10}px`;
     }
 
     handleDocPointerout = (e) => {
-
-
-
+      
+      if (!e.target.closest('[data-tooltip]')) return;
+      
+      this.element.remove();
     }
 
     handleDocPointermove = (e) => {
+      if (!e.target.closest('[data-tooltip]')) return;
 
-      
-
+      this.element.style.left = `${e.pageX + 10}px`;
+      this.element.style.top = `${e.pageY + 10}px`;
+    
     }
-
 
     destroy() {
 

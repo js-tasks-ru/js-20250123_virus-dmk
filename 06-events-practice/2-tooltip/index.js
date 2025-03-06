ff59a0aabd260826a7907f258559be68ba93d259
Dmitry <virus.dmk@gmail.com>
2025-03-05 17:56:04 -0800

    add tooltip realization: init, destroy, singleton, render

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,77 @@
 class Tooltip {
-  initialize () {
-
+  static inst;
+  element;
+  
+  constructor() {
+  
+    if(Tooltip.inst) return Tooltip.inst;
+  
+    Tooltip.inst = this;
+    this.createEventListeners();  
   }
-}
+  
+    initialize () {
+  
+      const el = document.createElement('div');
+      el.innerHTML = this.createTooltipTemplate();
+      this.element = el.firstElementChild;
 
-export default Tooltip;
+    }
+
+    render(content) {
+
+      this.element.textContent = content;
+      document.body.appendChild(this.element);
+
+    }
+
+    createTooltipTemplate() {
+
+      return `<div class="tooltip"></div>`
+
+    }
+
+    createEventListeners() {
+
+      document.body.addEventListener('pointerover', handleDocPointerover);
+      document.body.addEventListener('pointerout', handleDocPointerout);
+      document.body.addEventListener('pointermove', handleDocPointermove);
+
+    }
+
+    removeEventListeners() {
+
+      document.body.removeEventListener('pointerover', handleDocPointerover);
+      document.body.removeEventListener('pointerout', handleDocPointerout);
+      document.body.removeEventListener('pointermove', handleDocPointermove);
+
+    }
+
+    handleDocPointerover = (e) => {
+
+
+
+    }
+
+    handleDocPointerout = (e) => {
+
+
+
+    }
+
+    handleDocPointermove = (e) => {
+
+      
+
+    }
+
+
+    destroy() {
+
+      this.element.remove();
+      this.removeEventListeners();
+
+    }
+  }
+  
+  export default Tooltip;
